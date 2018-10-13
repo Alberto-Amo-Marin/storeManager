6e10e4a534fbad5113ec22ca23baa0450f191b0a
aamomari <alberto.amo-marin@capgemini.com>
2018-10-13 18:01:42 +0200

    [ADD] - Created Routes

--- storeManager/src/app/app-routing.module.ts
+++ storeManager/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainscreenComponent } from './mainScreen/mainscreen/mainscreen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',  component: MainscreenComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
