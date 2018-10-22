8fe99797e298bbec923d770a5028d4c9cbf828f5
aamomari <alberto.amo-marin@capgemini.com>
2018-10-22 15:55:10 +0200

    [TEST] - today date

--- storeManager/src/app/app.module.ts
+++ storeManager/src/app/app.module.ts
@@ -10,12 +10,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
 import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+
 
 
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
 import { FlexLayoutModule } from "@angular/flex-layout";
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+
+import { MatMomentDateModule } from "@angular/material-moment-adapter";
+
 
 
 
@@ -34,7 +42,13 @@ import { FlexLayoutModule } from "@angular/flex-layout";
     MatToolbarModule,
     MatCardModule,
     MatInputModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    FormsModule,
+    ReactiveFormsModule
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- storeManager/src/app/mainScreen/mainscreen/mainscreen.component.ts
+++ storeManager/src/app/mainScreen/mainscreen/mainscreen.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import * as moment from 'moment';
+import {MAT_MOMENT_DATE_FORMATS, MomentDateAdapter} from '@angular/material-moment-adapter';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
+
 
 @Component({
   selector: 'app-mainscreen',
   templateUrl: './mainscreen.component.html',
-  styleUrls: ['./mainscreen.component.css']
+  styleUrls: ['./mainscreen.component.css'],
+  providers: [
+    // `MomentDateAdapter` and `MAT_MOMENT_DATE_FORMATS` can be automatically provided by importing
+    // `MatMomentDateModule` in your applications root module. We provide it at the component level
+    // here, due to limitations of our example generation script.
+    {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
+    {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
+  ],
 })
 export class MainscreenComponent implements OnInit {
+  date = new Date();
+  date2 = new FormControl(moment().format('dddd'));
 
   constructor() { }
 
   ngOnInit() {
+    //var date = new Date();
+    //var date2 = new FormControl(moment(date,"DD/MM/YYYY"));
+    //var date = new FormControl(moment([2017, 0, 1]));
   }
 
 }
